f920c8c9c35714e1435b216847086c5762a5c53f
umairdalvi <dev.ud02@proton.me>
2025-11-21 12:06:30 +0530

    create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header"
 import 'remixicon/fonts/remixicon.css';
+import Navbar from "./components/Navbar";
+import './App.css'
 
 const App = () => {
   return (
     <div className="container mx-auto">
       <Header />
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="w-fit mx-auto mt-4">
+            <ul className='flex gap-8 font-medium'>
+                <li>
+                    <a href="#">Home<i className="ri-arrow-drop-down-line"></i></a>
+                </li>
+                <li>
+                    <a href="#">Shop<i className="ri-arrow-drop-down-line"></i></a>
+                </li>
+                <li>
+                    <a href="#">Products<i className="ri-arrow-drop-down-line"></i></a>
+                </li>
+                <li>
+                    <a href="#">Pages<i className="ri-arrow-drop-down-line"></i></a>
+                </li>
+                <li>
+                    <a href="#">Blog<i className="ri-arrow-drop-down-line"></i></a>
+                </li>
+                <li>
+                    <a href="#">Buy Theme!</a>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Navbar
