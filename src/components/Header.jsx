091ed206392b5e1daef17cf8be4bffc6b9098da2
umairdalvi <dev.ud02@proton.me>
2025-12-01 20:39:19 +0530

    update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 const Header = () => {
     return (
-        <div className="flex justify-between items-center py-4 px-4 border-b border-b-slate-300">
-            <div className="">
-                <div className="flex gap-5">
+        <div className="flex justify-between items-center p-2 lg:p-4 lg:border-b border-b-slate-300">
+            <div>
+                <div className="gap-5 hidden lg:flex">
                     <p className="cursor-pointer">+1 (973) 435-3638</p>
                     <p className="cursor-pointer">info@furniture.com</p>
                 </div>
+                <div className="lg:hidden text-xl">
+                    <i className="ri-menu-line"></i>
+                </div>
             </div>
-            <div className="-translate-x-14">
+            <div className="translate-x-4 md:translate-x-10 lg:-translate-x-14">
                 <div>
                     <a href="#">
                         <img
@@ -20,10 +23,18 @@ const Header = () => {
             </div>
             <div className="">
                 <div className="flex gap-4 text-xl">
-                    <i className="cursor-pointer ri-search-line"></i>
-                    <i className="cursor-pointer ri-user-3-line"></i>
-                    <i className="cursor-pointer ri-heart-line"></i>
-                    <i className="cursor-pointer ri-shopping-cart-line"></i>
+                    <div>
+                        <i className="cursor-pointer ri-search-line"></i>
+                    </div>
+                    <div className="hidden lg:inline-block">
+                        <i className="cursor-pointer ri-user-3-line"></i>
+                    </div>
+                    <div className="hidden lg:inline-block">
+                        <i className="cursor-pointer ri-heart-line"></i>
+                    </div>
+                    <div>
+                        <i className="cursor-pointer ri-shopping-cart-line"></i>
+                    </div>
                 </div>
             </div>
         </div>
