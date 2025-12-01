c82287c950ce4b95cf82162a9ef67edd713c8064
umairdalvi <dev.ud02@proton.me>
2025-12-01 20:40:21 +0530

    update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
     return (
-        <div className="w-fit mx-auto mt-4">
-            <ul className='flex gap-8 font-medium'>
+        <div className="w-fit mx-auto mt-4 hidden lg:block">
+            <ul className="flex gap-8 font-medium">
                 <li>
-                    <a href="#">Home<i className="ri-arrow-drop-down-line"></i></a>
+                    <a href="#">
+                        Home<i className="ri-arrow-drop-down-line"></i>
+                    </a>
                 </li>
                 <li>
-                    <a href="#">Shop<i className="ri-arrow-drop-down-line"></i></a>
+                    <a href="#">
+                        Shop<i className="ri-arrow-drop-down-line"></i>
+                    </a>
                 </li>
                 <li>
-                    <a href="#">Products<i className="ri-arrow-drop-down-line"></i></a>
+                    <a href="#">
+                        Products<i className="ri-arrow-drop-down-line"></i>
+                    </a>
                 </li>
                 <li>
-                    <a href="#">Pages<i className="ri-arrow-drop-down-line"></i></a>
+                    <a href="#">
+                        Pages<i className="ri-arrow-drop-down-line"></i>
+                    </a>
                 </li>
                 <li>
-                    <a href="#">Blog<i className="ri-arrow-drop-down-line"></i></a>
+                    <a href="#">
+                        Blog<i className="ri-arrow-drop-down-line"></i>
+                    </a>
                 </li>
                 <li>
                     <a href="#">Buy Theme!</a>
                 </li>
             </ul>
-
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
